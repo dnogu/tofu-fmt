616f9fa874a6cf444079ef0a4129d6d605ba6838
DN <DN@DN.local>
2025-08-30 08:49:48 -0400

    fix: Add missing ESLint configuration file
    Adds .eslintrc.js with Node.js and Jest environment configuration to resolve
    CI linting failures. Uses recommended ESLint rules with appropriate settings
    for GitHub Actions development.
    
    Resolves: ESLint couldn't find a configuration file error in CI

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: false,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    'no-console': 'off'
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    'coverage/'
+  ]
+};
